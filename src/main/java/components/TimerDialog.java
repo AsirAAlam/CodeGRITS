05ae4ae55f942c064d1a7c95e8bfc1a3ca9abd05
Miguel Rosario <miguelangel53013@gmail.com>
2024-11-17 23:38:35 -0500

    Polished dialog box and displayed text in box

--- src/main/java/components/TimerDialog.java
+++ src/main/java/components/TimerDialog.java
@@ -1,2 +1,32 @@
-package components;public class TimerDialog {
+
+package components;
+
+import com.intellij.openapi.ui.DialogWrapper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TimerDialog extends DialogWrapper {
+    private Timer timer;
+
+    public TimerDialog() {
+        super(true); // use current window as parent
+        setTitle("Timer");
+        setModal(false);
+        init();
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        JPanel dialogPanel = new JPanel(new BorderLayout());
+
+        JLabel timerDisplay = new JLabel("Time: 0:00", SwingConstants.CENTER);
+        // timerDisplay.setPreferredSize(new Dimension(100, 100));
+        dialogPanel.add(timerDisplay, BorderLayout.CENTER);
+
+        return dialogPanel;
+    }
 }
