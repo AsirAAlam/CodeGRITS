00a502f00cd9822fd4aeaed87a3a60f47d0d04bb
Miguel Rosario <miguelangel53013@gmail.com>
2024-11-19 19:36:52 -0500

    Added timer component that starts counting on tracking start

--- src/main/java/components/TimerDialog.java
+++ src/main/java/components/TimerDialog.java
@@ -7,9 +7,10 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TimerDialog extends DialogWrapper {
-    private Timer timer;
 
     public TimerDialog() {
         super(true); // use current window as parent
@@ -24,7 +25,20 @@ public class TimerDialog extends DialogWrapper {
         JPanel dialogPanel = new JPanel(new BorderLayout());
 
         JLabel timerDisplay = new JLabel("Time: 0:00", SwingConstants.CENTER);
-        // timerDisplay.setPreferredSize(new Dimension(100, 100));
+        long start = System.currentTimeMillis();
+
+        Timer timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                long timeFromStart = System.currentTimeMillis() - start;
+                int seconds = (int) (timeFromStart / 1000);
+                int minutes = seconds / 60;
+                seconds = seconds % 60;
+
+                timerDisplay.setText(String.format("Time: %d:%02d", minutes, seconds));
+            }
+        });
+        timer.start();
         dialogPanel.add(timerDisplay, BorderLayout.CENTER);
 
         return dialogPanel;
