2a282571bdc1d28dc45d2a3b1dca08e31dde060d
Miguel Rosario <miguelangel53013@gmail.com>
2024-11-24 22:59:21 -0500

    Removed excessive import from PauseResumeTrackingAction

--- src/main/java/actions/PauseResumeTrackingAction.java
+++ src/main/java/actions/PauseResumeTrackingAction.java
@@ -2,7 +2,6 @@ package actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import components.TimerDialog;
 import org.jetbrains.annotations.NotNull;
 import trackers.ScreenRecorder;
